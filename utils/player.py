817b02fd19637eaabdef5d83bbd5012e0ff2002f
Msdot001 <masudowolabi@gmail.com>
2022-02-21 16:49:18 +0100

    first_update

---
+++ utils/player.py
@@ -0,0 +1,19 @@
+from card import Card
+from random import sample
+
+"""A sett of class that can be used to represent a card"""
+
+class Player:
+
+    def __init__(self,cards,turn_count,number_of_count,number_of_cards,history):
+        """Initializing the attribute of class Player"""
+        Card.__init__(color, icon, value)
+        self.turn_count = turn_count
+        self.number_of_count = number_of_count
+        self.number_of_cards = number_of_cards
+        self.history = history
+
+
+    def play(self):
+        random.choice(Cards)
+
